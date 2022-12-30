7eb663fdc0f53be6ac4dfab720c067c829b8b6f1
michael <michael@example.com>
2022-12-30 19:50:50 +0100

    Debug change

--- Blog/settings.py
+++ Blog/settings.py
@@ -140,7 +140,7 @@ from platformshconfig import Config
 config = Config()
 if config.is_valid_platform():
     ALLOWED_HOSTS.append('.platformsh.site')
-    DEBUG = False
+    DEBUG = True
     
 
     if config.appDir:
